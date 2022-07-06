a6d335079b4507d62bfda1223fbf21e84f63a8d5
rezafahri11 <108709559+rezafahri11@users.noreply.github.com>
2022-07-06 16:57:36 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:try_app/detail_page.dart';
 import 'package:try_app/list_page.dart';
+import 'package:try_app/restaurant.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,12 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Restaurant",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home : RestaurantListPage()
-    );
+        title: "Restaurant",
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        initialRoute: RestaurantListPage.routeName,
+        routes: {
+          RestaurantListPage.routeName: (context) => RestaurantListPage(),
+          RestaurantDetailPage.routeName: (context) => RestaurantDetailPage(
+              restaurant: ModalRoute.of(context)?.settings.arguments
+                  as RestaurantElement),
+        });
   }
 }
